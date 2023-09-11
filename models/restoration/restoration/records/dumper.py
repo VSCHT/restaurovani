a497bc8c16c3b91ac2c4c04880e4ef9fe1a2e802
Mirek Simek <miroslav.simek@gmail.com>
2023-09-11 17:08:35 +0200

    Committed automatically before model restoration has been compiled
    omb-00041-before-model-compile-restoration

--- models/restoration/restoration/records/dumper.py
+++
@@ -1,9 +0,0 @@
-from invenio_records.dumpers import SearchDumper
-
-
-class RestorationDumper(SearchDumper):
-    """RestorationRecord opensearch dumper."""
-
-
-class RestorationDraftDumper(SearchDumper):
-    """RestorationDraft opensearch dumper."""
--- models/restoration/restoration/resources/records/resource.py
+++
@@ -1,8 +0,0 @@
-from invenio_drafts_resources.resources import RecordResource
-
-
-class RestorationResource(RecordResource):
-    """RestorationRecord resource."""
-
-    # here you can for example redefine
-    # create_url_rules function to add your own rules
