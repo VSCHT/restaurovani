464590e197c4d56a04d064db814d0558516d631e
AlbinaMakisheva <allielane24@gmail.com>
2023-09-18 21:06:38 +0200

    searchbar + validation

--- ui/restoration-ui/restoration_ui/theme/assets/semantic-ui/js/restoration_ui/search/components/MultipleSearchBarElement.jsx
+++ ui/restoration-ui/restoration_ui/theme/assets/semantic-ui/js/restoration_ui/search/components/MultipleSearchBarElement.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import { MultipleOptionsSearchBarCmp } from "@js/invenio_search_ui/components";
+import { MultipleOptionsSearchBarRSK } from "@js/invenio_search_ui/components";
+import { SearchBar } from "@js/invenio_search_ui/components";
+
+
 import { i18next } from "@translations/restoration_ui/i18next";
 import { withState, buildUID } from "react-searchkit";
 import { SearchConfigurationContext } from "@js/invenio_search_ui/components";
@@ -7,23 +10,29 @@ import Overridable from "react-overridable";
 import { Input } from "semantic-ui-react";
 
 
+
+console.log(SearchBar)
+
+
 export const MultipleSearchBarElement= ({ queryString, onInputChange, appName}) => {
-  const headerSearchbar = document.getElementById("header-search-bar");
-  const searchbarOptions = JSON.parse(headerSearchbar.dataset.options);
-  console.log(headerSearchbar)
-  console.log(searchbarOptions)
+ const headerSearchbar = document.getElementById("header-search-bar");
+ const searchbarOptions = JSON.parse(headerSearchbar.dataset.options);
+ console.log(headerSearchbar)
+ console.log(searchbarOptions)
 
-  return (
-    <>
-    <Input></Input>
-    </>
-    // <MultipleOptionsSearchBarCmp
-    //   options={searchbarOptions}
-    //   onInputChange={onInputChange}
-    //   queryString={queryString}
-    //   placeholder={`${i18next.t("Search")}...`}
-    // />
 
-   
-  );
+ return (
+  
+   <MultipleOptionsSearchBarRSK
+     options={searchbarOptions}
+     onInputChange={onInputChange}
+     queryString={queryString}
+     placeholder={`${i18next.t("Search")}...`}
+   />
+  // <SearchBar/>
+  
+
+
+ 
+ );
 };
