9f0009a0c6bd321af9d7f085e87f9688f886c0aa
Mirek Simek <miroslav.simek@gmail.com>
2023-11-14 13:36:22 +0100

    Committed automatically before model restoration has been compiled
    omb-00001-before-model-compile-restoration

--- ui/restoration-ui/restoration_ui/resources/config.py
+++ ui/restoration-ui/restoration_ui/resources/config.py
@@ -1,7 +1,6 @@
 from restoration_ui.resources.components import RestorationVocabularyOptionsComponent
 from oarepo_ui.resources.config import RecordsUIResourceConfig
-from oarepo_ui.resources import BabelComponent
-
+from oarepo_ui.resources.components import BabelComponent, FilesComponent
 
 
 class RestorationUiResourceConfig(RecordsUIResourceConfig):
@@ -12,23 +11,30 @@ class RestorationUiResourceConfig(RecordsUIResourceConfig):
     api_service = "restoration"
     layout = "restoration"
 
-    components = [BabelComponent, RestorationVocabularyOptionsComponent ]
-    
+    components = [BabelComponent, RestorationVocabularyOptionsComponent, FilesComponent]
+
     templates = {
         "detail": {
             "layout": "restoration_ui/Detail.html.jinja",
-            "blocks": {
-                "record_main_content": "Main",
-                "record_sidebar": "Sidebar"                
-            },
+            "blocks": {"record_main_content": "Main", "record_sidebar": "Sidebar"},
         },
-        "search": {"layout": "restoration_ui/Search.html.jinja", "app_id": "Restoration_ui.Search"},
+        "search": {
+            "layout": "restoration_ui/Search.html.jinja",
+            "app_id": "Restoration_ui.Search",
+        },
         "edit": {"layout": "restoration_ui/EditObjectForm.html.jinja"},
         "create": {"layout": "restoration_ui/CreateObjectForm.html.jinja"},
     }
+
     def search_app_config(self, identity, api_config, overrides={}, **kwargs):
-        return super().search_app_config(identity, api_config,overrides=overrides, endpoint='/api/user/restoration/', **kwargs)
-    
+        return super().search_app_config(
+            identity,
+            api_config,
+            overrides=overrides,
+            endpoint="/api/user/restoration/",
+            **kwargs
+        )
+
     def search_active_facets(self, api_config, identity):
         return [
             k
