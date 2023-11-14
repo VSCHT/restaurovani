77b9309bc9114d807ee5b0ada17cbdc563c03415
Mirek Simek <miroslav.simek@gmail.com>
2023-11-14 14:09:01 +0100

    Committed automatically after model restoration has been compiled
    omb-00006-after-model-compile-restoration

--- models/restoration/restoration/resources/files/resource.py
+++ models/restoration/restoration/resources/files/resource.py
@@ -1,15 +1,14 @@
-from invenio_records_resources.resources.files.resource import FileResource
-from oarepo_ui.resources.components import FilesComponent
+from oarepo_ui.resources.file_resource import S3RedirectFileResource
 
 
-class RestorationFileResource(FilesComponent):
+class RestorationFileResource(S3RedirectFileResource):
     """RestorationFile resource."""
 
     # here you can for example redefine
     # create_url_rules function to add your own rules
 
 
-class RestorationFileDraftResource(FileResource):
+class RestorationFileDraftResource(S3RedirectFileResource):
     """RestorationFileDraft resource."""
 
     # here you can for example redefine
