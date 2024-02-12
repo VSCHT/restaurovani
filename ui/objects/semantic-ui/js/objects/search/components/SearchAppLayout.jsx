f6a021e7d1e6786d87136224a0137738fbf5d421
AlbinaMakisheva <allielane24@gmail.com>
2024-02-12 10:01:52 +0100

    reduced less

--- ui/objects/semantic-ui/js/objects/search/components/SearchAppLayout.jsx
+++ ui/objects/semantic-ui/js/objects/search/components/SearchAppLayout.jsx
@@ -63,37 +63,31 @@ export const SearchAppLayout = () => {
       <Grid columns={2} className="double gapped">
         {/* first column-  main content */}
         <Grid.Column width={12}>
-
           {/* header and searchbar */}
           <Container>
             <Grid columns={2} className="spaced">
-
               <Grid.Column width={11}>
-              <Header as="h2">
-                Restaurované předměty
-              </Header>
-            </Grid.Column>
-
-            <Grid.Column width={4}>
-              <Grid.Row className="grid-searchbar">
-                <SearchBar />
-                <Button
-                  className="transparent filter"
-                  aria-label="Toggle Filter Menu"
-                  onClick={toggleModal}
-                >
-                  <Image
-                    rel="icon"
-                    src="/static/images/filter-icon.png"
-                    alt="filter button"
-                  />
-                </Button>
-              </Grid.Row>
+                <Header as="h2">Restaurované předměty</Header>
               </Grid.Column>
 
+              <Grid.Column width={4}>
+                <Grid.Row className="grid-searchbar">
+                  <SearchBar />
+                  <Button
+                    className="transparent filter"
+                    aria-label="Toggle Filter Menu"
+                    onClick={toggleModal}
+                  >
+                    <Image
+                      rel="icon"
+                      src="/static/images/filter-icon.png"
+                      alt="filter button"
+                    />
+                  </Button>
+                </Grid.Row>
+              </Grid.Column>
             </Grid>
           </Container>
-
 
           {/* search results */}
           <Grid.Column>
@@ -117,21 +111,7 @@ export const SearchAppLayout = () => {
               src="/static/images/logoVSCHT_zakl.png"
               alt="vsht logo"
             />
-
-            {/* <Button
-              className="transparent close"
-              aria-label="Toggle Filter Menu"
-              onClick={(e) => toggleSidebar(e)}
-            >
-              <Image
-                rel="icon"
-                src="/static/images/close-icon.png"
-                alt="burger filter button"
-              />
-            </Button> */}
-            <Grid.Column
-              aria-label="Filter Options"
-            >
+            <Grid.Column aria-label="Filter Options">
               <Button
                 secondary
                 aria-label="Tlacitko dodat novy predmet"
@@ -156,11 +136,9 @@ export const SearchAppLayout = () => {
           open={modalOpen}
           onClose={toggleModal}
         >
-          <Grid className="predmety__aside" id="predmety__aside">
-            <Grid.Column
-              aria-label="Filter Options"
-            >
-              <Header as='h3'>Filtrování výsledků</Header>
+          <Grid className="predmety__aside">
+            <Grid.Column aria-label="Filter Options">
+              <Header as="h3">Filtrování výsledků</Header>
               <SearchAppFacets aggs={searchAppConfig.aggs} appName={appName} />
             </Grid.Column>
             <Button
@@ -177,6 +155,6 @@ export const SearchAppLayout = () => {
           </Grid>
         </Modal>
       </Grid>
-      </Container>
+    </Container>
   );
 };
