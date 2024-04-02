005a0518c9c5bfb8c8ce5c88a2104763bd2d2e3f
AlbinaMakisheva <allielane24@gmail.com>
2024-04-02 17:50:31 +0200

    tests

--- tests/ui/title-page.spec.ts
+++ tests/ui/title-page.spec.ts
@@ -1,17 +1,15 @@
 import { test, expect } from "playwright/test";
 
-const url = "https://127.0.0.1:5000/";
-
 test("has title", async ({ page }) => {
-  await page.goto(url);
+  await page.goto('/');
   await expect(page).toHaveTitle(
     /Repozitář dokumentace restaurování uměleckých předmětů/
   );
 });
 
-test("search and check URL", async ({ page }) => {
-  await page.goto(url);
-  await page.getByRole("textbox", { name: "Vyhledat předmět" }).fill("sklo");
-  await page.getByRole("textbox", { name: "Vyhledat předmět" }).press("Enter");
-  await expect(page).toHaveURL(`${url}objekty/?q=sklo`);
+test("search and check URL", async ({ page, baseURL }) => {
+  await page.goto('/');
+  await page.locator(`.pusher .ui.input[name='q']`).fill("sklo");
+  await page.locator(`.pusher .ui.input[name='q']`).press("Enter");
+  await expect(page).toHaveURL(`${baseURL}objekty/?q=sklo`);
 });
