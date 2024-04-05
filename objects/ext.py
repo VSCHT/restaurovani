6e61dc4fdefa247e8bb40ba9a098c03e1a4f8a41
Alzpetka <lili3.pokorna@seznam.cz>
2024-04-05 14:40:51 +0200

    aggregation result type fix

--- objects/ext.py
+++ objects/ext.py
@@ -1,6 +1,6 @@
 import re
 from functools import cached_property
-
+from oarepo_requests.proxies import current_oarepo_requests_service
 from oarepo_requests.resources.draft.config import DraftRecordRequestsResourceConfig
 
 from objects import config
@@ -62,7 +62,8 @@ class ObjectsExt:
     @cached_property
     def service_requests(self):
         return config.OBJECTS_REQUESTS_SERVICE_CLASS(
-            record_service=self.service_records
+            record_service=self.service_records,
+            oarepo_requests_service=current_oarepo_requests_service
         )
 
     @cached_property
