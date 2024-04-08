f6a5f9412d04c7b067102bac67feedd4a183f710
AlbinaMakisheva <allielane24@gmail.com>
2024-04-08 08:59:06 +0200

    vocab

--- ui/objects/semantic-ui/js/objects/editform/components/VocabularyTreeSelectField.jsx
+++ ui/objects/semantic-ui/js/objects/editform/components/VocabularyTreeSelectField.jsx
@@ -19,7 +19,6 @@ import {
   ModalActions,
 } from "semantic-ui-react";
 import {
-  serializedVocabularyItems,
   processVocabularyItems ,
   serializeVocabularyItem,
 } from "@js/oarepo_vocabularies";
